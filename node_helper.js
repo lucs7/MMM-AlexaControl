2d4e183afb5ee44cde86909accd3900979e47f11
lucs7 <s0810498@gmail.com>
2024-12-07 19:59:54 +0100

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -249,11 +249,11 @@ module.exports = NodeHelper.create({
 	    else if(this.config.vcgencmd =='xrandr'){
                 device.handler = function(action) {     
                     if(action === 1){
-                        exec("xrandr --output HDMI-1 --auto", opts, (error, stdout, stderr) => {
+                        exec("xrandr --output default --auto", opts, (error, stdout, stderr) => {
                             _this.checkForExecError(error, stdout, stderr); 
                         });
                     }if(action === 0){
-                        exec("xrandr --output HDMI-1 --off", opts, (error, stdout, stderr) => {
+                        exec("xrandr --output default --off", opts, (error, stdout, stderr) => {
                             _this.checkForExecError(error, stdout, stderr); 
                         });
                     }
